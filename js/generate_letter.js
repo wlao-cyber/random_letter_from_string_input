f59b881e081f68824e1ab41eb2d792d4cd27833c
Wilson Lao <68834234+wlao-cyber@users.noreply.github.com>
2023-01-25 03:33:43 -0800

    Add files via upload

--- js/generate_letter.js
+++ js/generate_letter.js
@@ -7,12 +7,15 @@ export function generate_letter()
 
     // To get the letter within a string, set to its index.
     // a string has the maximum index of its length minus 1
-    // multiply by Math.random to get probability from 0 to 100% of getting the maximum number 
-    // Then chain with Math.floor to round down.
+    // multiply by Math.random to get probability between 0 to 100% (never 0 and never 1) of getting the maximum number 
+    // Math.floor rounds down to the integer. Even 6.9999 will round down to 6
+    // Chain Math.floor with Math.random to get the index which will never be at or exceeed the length of the strength.
 
     // Can also use .charAt() instead of calling an index of the variable.
     // There are two ways to access an individual character in a string. 
     // The first is the charAt method, part of ECMAScript 3. 
     // And the second way, standardizeed in ECMAScript 5, is treating a string as an array-like object, in which each individual character corresponds to a numerical index.
-    document.getElementById("console").value = name_input[Math.floor((name_input.length - 1)*Math.random())]
+
+    document.getElementById("console").value = name_input[Math.floor((name_input.length)*Math.random())]
+    // console.log(name_input[Math.floor((name_input.length)*1)])
 }
